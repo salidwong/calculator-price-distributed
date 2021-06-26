bc8700d2f883ee5b989fc9ca9550f98a45ab391c
Salidwong Wansanoh <salidwong.wa@gettgo.com>
2021-06-26 15:51:14 +0700

    [frontend] initial project
    - Add redux and redux-toolkit
    - Add google-map-react
    - Add material-ui
    - Add react-number-format

---
+++ src/features/Calculator/index.js
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+import { default as NumberFormat } from "react-number-format";
+
+export const Calculator = () => {
+  return (
+    <>
+      <Typography>Test</Typography>
+      <NumberFormat value="20000" />
+    </>
+  );
+};
---
+++ src/features/MapModal/index.js
@@ -0,0 +1,23 @@
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => (
+  <div onClick={() => console.log("on Click")}>{text}</div>
+);
+
+export const MapModal = () => {
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDqyHbnhdks8FRGsscPc3adtV2Dt-LXEqc" }}
+        defaultCenter={{
+          lat: 13.7563,
+          lng: 100.5018,
+        }}
+        defaultZoom={11}
+      >
+        <AnyReactComponent lat={13.7370587} lng={100.5603061} text="Asoke" />
+      </GoogleMapReact>
+    </div>
+  );
+};
